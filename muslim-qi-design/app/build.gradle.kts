133b2a779ff0b23c38e40e39f48d5917e9e732a7
juankahf24 <juanka.hf.24@gmail.com>
2026-08-02 21:21:35 +0200

    chore: bump Muslim QI design to 0.4.0

--- muslim-qi-design/app/build.gradle.kts
+++ muslim-qi-design/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.muslimqi.design"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0-design"
+        versionCode = 4
+        versionName = "0.4.0-design"
     }
 
     buildFeatures {
